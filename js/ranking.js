f6f806345fd7123d1e578a5eac73d105b46ad6de
yangclaw2026 <yangclaw2026@gmail.com>
2026-08-28 16:14:33 +0800

    Update ranking.js

--- js/ranking.js
+++ js/ranking.js
@@ -1,10 +1,10 @@
 var rule = {
     类型: '影视',
     title: '📊 豆瓣榜单',
-    host: 'https://raw.githubusercontent.com/yangclaw2026-png/tvbox-home/main/data',
+    host: 'https://caiji.dbzy5.com',
     
-    class_name: '电影排行&电视剧排行&综艺排行',
-    class_url: 'movies&tv&variety',
+    class_name: '电影&电视剧&综艺',
+    class_url: '1&2&3',
     
     searchable: 1,
     quickSearch: 1,
@@ -18,46 +18,40 @@ var rule = {
     limit: 50,
     
     推荐: async function () {
-        let url = this.host + '/movies.json';
+        let url = this.host + '/api.php/provide/vod/at/json/?ac=detail&pg=1';
         let html = await request(url);
         let data = JSON.parse(html);
-        return data.slice(0, 50).map(item => ({
+        return data.list.slice(0, 50).map(item => ({
             vod_id: item.vod_id,
             vod_name: item.vod_name,
             vod_pic: item.vod_pic,
-            vod_remarks: item.vod_remarks || item.score + '分'
+            vod_remarks: item.vod_douban_score ? item.vod_douban_score + '分' : item.vod_remarks
         }));
     },
     
     一级: async function (tid, pg, filter, extend) {
-        let url = this.host + `/${tid}.json`;
+        let url = this.host + `/api.php/provide/vod/at/json/?ac=detail&t=${tid}&pg=${pg}`;
         let html = await request(url);
         let data = JSON.parse(html);
         return data.list.map(item => ({
             vod_id: item.vod_id,
             vod_name: item.vod_name,
             vod_pic: item.vod_pic,
-            vod_remarks: item.vod_remarks
+            vod_remarks: item.vod_douban_score ? item.vod_douban_score + '分' : item.vod_remarks
         }));
     },
     
     二级: async function (ids) {
-        let files = ['movies.json', 'tv.json', 'variety.json'];
-        let vod = null;
-        for (let file of files) {
-            let url = this.host + `/${file}`;
-            let html = await request(url);
-            let data = JSON.parse(html);
-            vod = data.find(item => item.vod_id == ids[0]);
-            if (vod) break;
-        }
-        if (!vod) return {list: []};
+        let url = this.host + `/api.php/provide/vod/at/json/?ac=detail&ids=${ids[0]}`;
+        let html = await request(url);
+        let data = JSON.parse(html);
+        let vod = data.list[0];
         return {
             list: [{
                 vod_id: vod.vod_id,
                 vod_name: vod.vod_name,
                 vod_pic: vod.vod_pic,
-                vod_content: vod.content || '',
+                vod_content: vod.vod_content || '',
                 vod_play_from: vod.vod_play_from || 'default',
                 vod_play_url: vod.vod_play_url || ''
             }]
@@ -65,23 +59,15 @@ var rule = {
     },
     
     搜索: async function (wd, quick, pg) {
-        let results = [];
-        let files = ['movies.json', 'tv.json', 'variety.json'];
-        for (let file of files) {
-            let url = this.host + `/${file}`;
-            let html = await request(url);
-            let data = JSON.parse(html);
-            let filtered = data.filter(item => 
-                item.vod_name && item.vod_name.includes(wd)
-            );
-            results = results.concat(filtered.map(item => ({
-                vod_id: item.vod_id,
-                vod_name: item.vod_name,
-                vod_pic: item.vod_pic,
-                vod_remarks: item.vod_remarks
-            })));
-        }
-        return results;
+        let url = this.host + `/api.php/provide/vod/at/json/?ac=detail&wd=${encodeURIComponent(wd)}&pg=${pg}`;
+        let html = await request(url);
+        let data = JSON.parse(html);
+        return data.list.map(item => ({
+            vod_id: item.vod_id,
+            vod_name: item.vod_name,
+            vod_pic: item.vod_pic,
+            vod_remarks: item.vod_douban_score ? item.vod_douban_score + '分' : item.vod_remarks
+        }));
     },
     
     lazy: async function (flag, id) {
